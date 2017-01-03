bb77669f59dc24d28b45a33e03a0d990978d784c
Andrea Casaccia <andrea@cainthus.com>
2017-01-03 14:56:00 +0100

    Add support for API_URL environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ var webpack = require('webpack');
 var plugins = [
     new webpack.DefinePlugin({
         "process.env": {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+            API_URL: JSON.stringify(process.env.API_URL)
         }
     })
 ];
